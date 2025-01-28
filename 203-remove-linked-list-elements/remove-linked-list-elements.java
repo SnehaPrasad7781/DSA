3b07e3fd16d9c71de10a27a43e26a45ce642f65c
SnehaPrasad7781 <119395321+SnehaPrasad7781@users.noreply.github.com>
2025-01-28 16:28:54 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (66.88%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.java
+++ 203-remove-linked-list-elements/remove-linked-list-elements.java
@@ -8,23 +8,37 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode removeElements(ListNode head, int val) {
+//         while(head != null && head.val == val){
+//             head = head.next;
+//         }
+//         ListNode temp=head;
+//         ListNode temp2=(temp!= null) ? temp.next : null;
+//         while(temp!=null && temp2!= null){
+//             if(temp2.val==val){
+//                 temp.next=temp2.next;
+//                 temp2 = temp2.next;
+//             }
+//             else{
+//                 temp = temp.next;
+//                 temp2 = temp2.next;
+//             }
+//         }
+//         return head;
+//     }
+// }
+
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        while(head != null && head.val == val){
-            head = head.next;
+        if (head == null) {
+            return null; 
         }
-        ListNode temp=head;
-        ListNode temp2=(temp!= null) ? temp.next : null;
-        while(temp!=null && temp2!= null){
-            if(temp2.val==val){
-                temp.next=temp2.next;
-                temp2 = temp2.next;
-            }
-            else{
-                temp = temp.next;
-                temp2 = temp2.next;
-            }
+        if (head.val == val) {
+            return removeElements(head.next, val);
+        } else {
+            head.next = removeElements(head.next, val);
+            return head;
         }
-        return head;
     }
 }
